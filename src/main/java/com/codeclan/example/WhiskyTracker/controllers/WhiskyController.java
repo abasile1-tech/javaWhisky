ccefdac878d9d90bb14d21669a9df3bbdd5a5e9a
James Dickson <jamesstuartdickson@gmail.com>
2023-10-03 15:13:08 +0100

    Added code to whisky controller, however getting 404 in insomnia

--- src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
@@ -1,9 +1,34 @@
 package com.codeclan.example.WhiskyTracker.controllers;
 
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class WhiskyController {
+    @Autowired
+    WhiskyRepository whiskyRepository;
+
+    @GetMapping(name = "/whiskies")
+    public ResponseEntity<List<Whisky>> getWhiskies(){
+        return new ResponseEntity<>(whiskyRepository.findAll(), HttpStatus.OK);
+    }
+
+//    @GetMapping(name = "/whiskies")
+//    public ResponseEntity<List<Whisky>> getWhiskies(@RequestParam(name = "year", required = false)int year){
+//        if (year == 0) {
+//            return new ResponseEntity<>(whiskyRepository.findAll(), HttpStatus.OK);
+//        } else {
+//            return new ResponseEntity<>(whiskyRepository.findByYear(year), HttpStatus.OK);
+//        }
+//    }
 
 }
