0cfc9434e00739d850fa43e4f71f1d31372cbeb9
John <ashton.basile@gmail.com>
2023-10-03 12:27:08 +0100

    first commit

---
+++ src/main/java/com/example/javaWhisky/JavaWhiskyApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaWhisky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWhiskyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWhiskyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javaWhisky/JavaWhiskyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaWhisky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWhiskyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
